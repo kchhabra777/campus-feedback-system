c34388a8ba67aabd5541e03696e7060dc2b0a379
Krishna <krishcarbon7@gmail.com>
2026-09-01 08:08:21 +0530

    Remove Neon Serverless Adapter in favor of standard Prisma Postgres connection for EC2 Node environment

--- feedback-microservice/src/lib/prisma.js
+++ feedback-microservice/src/lib/prisma.js
@@ -1,17 +1,6 @@
 import "dotenv/config";
-
 import { PrismaClient } from "@prisma/client";
-import { PrismaNeon } from "@prisma/adapter-neon";
 
-
-const adapter = new PrismaNeon({
-    connectionString: process.env.DATABASE_URL
-});
-
-
-const prisma = new PrismaClient({
-    adapter
-});
-
+const prisma = new PrismaClient();
 
 export default prisma;
